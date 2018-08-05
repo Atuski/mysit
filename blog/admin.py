e3762274315eb2bb3584a8538ff90719840bea99
Atuski <37727483+Atuski@users.noreply.github.com>
2018-08-05 21:29:55 +0800

    add read_num list

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Blog,BlogType
 # Register your models here.
 @admin.register(Blog)
 class BlogsAdmin(admin.ModelAdmin):
-	list_display = ('id','title','content','author','created_time','last_updatetime','blog_type')
+	list_display = ('id','title','content','author','read_num','created_time','last_updatetime','blog_type')
 	list_editable =('content',)
 	# search_fields = ('last_updatetime',)
 	# list_filter = ('title','id',)
